430d950e0836ecf7d96400e0dbc2aa6d088c61fe
RoyceYeh <royz.reus@gmail.com>
2022-01-07 20:15:40 +0800

    homework

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,204 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+	<section class="todo">
+		<div class="input-group mb-3">
+			<div class="input-group-prepend">
+				<span class="input-group-text" id="basic-addon1">待辦事項</span>
+			</div>
+			<input
+				type="text"
+				class="form-control"
+				placeholder="準備要做的任務"
+				v-model="newTodo"
+				@keyup.enter="addTodo"
+			/>
+			<div class="input-group-append">
+				<button class="btn btn-primary" type="button" @click="addTodo">
+					新增
+				</button>
+			</div>
+		</div>
+		<div class="card text-center">
+			<div class="card-header">
+				<ul class="nav nav-tabs card-header-tabs">
+					<li class="nav-item">
+						<a
+							class="nav-link"
+							:class="{ active: visibility == 'all' }"
+							@click="visibility = 'all'"
+							href="#"
+							>全部</a
+						>
+					</li>
+					<li class="nav-item">
+						<a
+							class="nav-link"
+							:class="{ active: visibility == 'active' }"
+							@click="visibility = 'active'"
+							href="#"
+							>進行中</a
+						>
+					</li>
+					<li class="nav-item">
+						<a
+							class="nav-link"
+							:class="{ active: visibility == 'completed' }"
+							@click="visibility = 'completed'"
+							href="#"
+							>已完成</a
+						>
+					</li>
+				</ul>
+			</div>
+			<ul class="list-group list-group-flush text-left">
+				<li
+					class="list-group-item"
+					v-for="item in filterTodos"
+					v-bind:key="item.id"
+					@dblclick="edit(item)"
+				>
+					<!-- 正常狀況都沒點擊的話，editTodo為空的物件，item.id !== editTodo.id 自然為True -->
+					<div class="d-flex" v-if="item.id !== editTodo.id">
+						<div class="form-check">
+							<input
+								type="checkbox"
+								class="form-check-input"
+								:id="item.id"
+								v-model="item.completed"
+							/>
+							<label
+								class="form-check-label"
+								:class="{ completed: item.completed }"
+								:for="item.id"
+							>
+								{{ item.title }}
+							</label>
+						</div>
+						<button
+							type="button"
+							class="close ml-auto"
+							aria-label="Close"
+							@click="removeTodo(item)"
+						>
+							<span aria-hidden="true">&times;</span>
+						</button>
+					</div>
+					<input
+						type="text"
+						class="form-control"
+						v-if="item.id == editTodo.id"
+						@keyup.esc="cancelEdit()"
+						@keyup.enter="doneEdit(item)"
+						v-model="editTodo.title"
+					/>
+				</li>
+				<!-- <li class="list-group-item">
+                  <input type="text" class="form-control" />
+                </li> -->
+			</ul>
+			<div class="card-footer d-flex justify-content-between">
+				<span>還有 3 筆任務未完成</span>
+				<a href="#">清除所有任務</a>
+			</div>
+		</div>
+	</section>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+	export default {
+		name: "HelloWorld",
+		// props: {
+		// 	msg: String,
+		// },
+		data() {
+			return {
+				newTodo: "",
+				todos: [],
+				editTodo: {},
+				editTitle: "",
+				visibility: "all",
+			};
+		},
+		methods: {
+			addTodo() {
+				//使用者輸入內容
+				//trim()刪除空白
+				let value = this.newTodo.trim();
+				//id
+				let timesTamp = Math.floor(Date.now());
+				//如果空白，則不能新增
+				if (!value) {
+					return;
+				}
+				console.log(value, timesTamp);
+				//新增物件進todos
+				this.todos.push({
+					id: timesTamp,
+					title: value,
+					completed: false,
+				});
+				//輸入後清除
+				this.newTodo = "";
+			},
+
+			removeTodo(item) {
+				let newIndex = this.todos.findIndex(function (task) {
+					return item.id === task.id;
+				});
+
+				this.todos.splice(newIndex, 1);
+			},
+
+			edit(item) {
+				console.log(item);
+				this.editTodo = item;
+				this.editTitle = item.title;
+			},
+
+			cancelEdit() {
+				this.editTodo = {};
+			},
+
+			doneEdit(item) {
+				item.title = this.editTodo.title;
+				this.editTitle = ""; //輸入完清空
+				this.editTodo = {};
+				//沒有雙擊任何一個todo的狀況下，editTodo本來就是空的，雙擊後才會出現input，所以在input編輯完後，要跳回原本checkbox的樣子，要回到editTodo是空的狀態。
+			},
+		},
+		computed: {
+			//過濾後的todos
+			filterTodos() {
+				let activeTodos = [];
+				if (this.visibility == "all") {
+					activeTodos = this.todos;
+				} else if (this.visibility == "active") {
+					//接收未完成的內容
+					this.todos.forEach(function (item) {
+						if (item.completed == false) {
+							activeTodos.push(item);
+						}
+					});
+				} else if (this.visibility == "completed") {
+					//接收未完成的內容
+					this.todos.forEach(function (item) {
+						if (item.completed == true) {
+							activeTodos.push(item);
+						}
+					});
+				}
+				return activeTodos;
+			},
+		},
+	};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+	.todo {
+		max-width: 600px;
+		margin: 0 auto;
+	}
+	.completed {
+		text-decoration: line-through;
+	}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,55 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+// import router from "./router";
+// import AuthPlugin from "./plugins/auth";
 
-Vue.config.productionTip = false
+import ApolloClient from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+
+import VueApollo from "vue-apollo";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+// Vue.use(AuthPlugin);
+Vue.use(VueApollo);
+Vue.config.productionTip = false;
+const getHeaders = () => {
+	const headers = {};
+	const token = window.localStorage.getItem("apollo-token");
+	if (token) {
+		headers.authorization = `Bearer ${token}`;
+	}
+	return headers;
+};
+
+// Create an http link:
+const link = new HttpLink({
+	uri: "https://hasura.io/learn/graphql",
+	fetch,
+	headers: getHeaders(),
+});
+
+const client = new ApolloClient({
+	link: link,
+	cache: new InMemoryCache({
+		addTypename: true,
+	}),
+});
+const apolloProvider = new VueApollo({
+	defaultClient: client,
+});
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue);
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+	apolloProvider,
+	render: (h) => h(App),
+}).$mount("#app");
